b6a81cf79ab67fedc90dfda8e882ba5ce9feb96f
Adonis0923 <91833579+Adonis0923@users.noreply.github.com>
2022-02-10 00:26:14 +0900

    add get question list function, add vote event

--- Ballot.sol
+++ Ballot.sol
@@ -5,8 +5,6 @@ import "hardhat/console.sol";
 
 contract Ballot {
   
-  uint constant UP = 1;
-  uint constant DOWN = 2;
   struct Voter {
     mapping(uint => bool) voted;
     address addr;
@@ -14,8 +12,8 @@ contract Ballot {
 
   struct Question {
     string value;
-    uint voteUpCount;
-    uint voteDownCount;
+    uint upCount;
+    uint downCount;
   }
 
   uint256 public voterCount;
@@ -26,30 +24,70 @@ contract Ballot {
   mapping(address => Voter) public voters;
 
   Question[] public questions;
+  
+  event Voted(address voter, uint256 to, bool value);
 
   constructor() {
+
     voterCount = 0;
     questionCount = 0;
   }
+
   function addQuestion(string memory v) public {
-    questions.push(Question({ value: v, voteUpCount: 0, voteDownCount: 0 }));
+
+    questions.push(Question({ value: v, upCount: 0, downCount: 0 }));
     questionCount = questionCount + 1;
+
+    console.log(questionCount);
   }
+
   function addVoter(address _voter) public {
+
     voters[_voter].addr = _voter;
     voterCount ++;
   }
-  function vote(uint id, uint value) public payable {
+
+  function vote(uint id, bool value) public payable {
+
     require(voters[msg.sender].voted[id] == false, "already voted");
-    if(value == UP) {
-        questions[id].voteUpCount ++;
+
+    if(value) {
+        questions[id].upCount ++;
     } else {
-        questions[id].voteDownCount ++;
+        questions[id].downCount ++;
     }
+
     voters[msg.sender].voted[id] = true;
+    emit Voted(msg.sender, id, value);
   }
-  function winningQuestion() public pure returns(uint winningQuestion_) {
-    uint p = 0;
-    winningQuestion_ = p;
+
+  function winningQuestion() public view returns(uint winningQuestion_) {
+    // int a = 2 - 5;
+    // console.log(a);
+    // uint p = 0;
+    // require(questionCount > 0, "no question");
+    // if(questionCount == 1) return 0;
+    
+    // Question memory win = questions[p];
+    // Question memory q;
+    // for(uint i = 1; i < questionCount; i++) {
+    //   q = questions[i];
+    //   int sum = int(win.upCount) - int(win.downCount);
+    //   int _sum = int(q.upCount) - int(q.downCount);
+    //   if(_sum > sum) p = i;
+    // }
+    // winningQuestion_ = p;
+  }
+
+  function getQuestions() public returns(string[] memory) {
+    
+    require(questionCount > 0, "not question exist");
+
+    string[] memory list = new string[](questionCount);
+
+    for(uint i = 0; i < questionCount; i++) {
+      list[i] = questions[i].value;
+    }
+    return list;
   }
 }
